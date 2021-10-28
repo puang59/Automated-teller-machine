61ac9947a02459b0726ceec84ee7e24dde425b7f
rezuwii <59052194+rezuwii@users.noreply.github.com>
2021-10-28 23:09:08 +0530

    Initial Commit

---
+++ start.cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  start.cpp
+//
+//  Created by Karan Kumar on 28/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
